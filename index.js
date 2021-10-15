9cfd71df216a8fe7ed7fbdc04da74ff1d85e6434
Martin Achtermann <65299501+roadpilot@users.noreply.github.com>
2021-10-15 15:31:30 +0000

    work 1

--- index.js
+++ index.js
@@ -8,3 +8,25 @@ To perform a flood fill, consider the starting pixel, plus any pixels connected 
 
 Return the modified image after performing the flood fill.
 */
+
+var floodFill = function(image, sr, sc, newColor) {
+    // sr = over
+    // sc = down
+    let a = image[-1]
+    if  (typeof image[-1] === 'undefined' || typeof image[0][-1] === 'undefined') return
+    if (image[sr-1][sc-1]===image[sr][sc]) image[sr-1][sc-1]=newColor
+    if (image[sr+1][sc-1]===image[sr][sc]) image[sr+1][sc-1]=newColor
+    if (image[sr-1][sc+1]===image[sr][sc]) image[sr-1][sc+1]=newColor
+    if (image[sr+1][sc+1]===image[sr][sc]) image[sr+1][sc+1]=newColor
+        
+//         image[0][1]=2
+//             if (image[0][0]===image[sr][sc]) image[0][0]=2
+//             image[0][2]=2
+
+//         image[1][0]=2
+//             image[0][0]=2
+//             image[2][0]=2
+
+    image[sr][sc]=2
+    return image
+};
